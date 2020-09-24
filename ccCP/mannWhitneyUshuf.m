3f720db37bde94f3b58afeea19967acde787a5cd
Nick Steinmetz <nick.steinmetz@gmail.com>
2020-09-24 09:17:42 -0700

    Update mannWhitneyUshuf.m

--- ccCP/mannWhitneyUshuf.m
+++ ccCP/mannWhitneyUshuf.m
@@ -5,7 +5,7 @@ function numer = mannWhitneyUshuf(x,y,shufLabels)
 % numer is the number of instances for which x>y, of all possible
 % comparisons. Divide by nx*ny for mannWhitney u statistic.
 %
-% shufLabels is ny x nshuf, each column a random permutation of integers
+% shufLabels is nx x nshuf, each column a random permutation of integers
 % from 1:nx. First column of shufLabels should be exactly 1:nx
 %
 % x and y are vectors
